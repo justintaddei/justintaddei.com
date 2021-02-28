71946137c8fe611d2d0dc3b54c8c14f01f1670ed
Justin Taddei <justin@justintaddei.com>
2021-02-27 19:11:34 -0700

    Add missing google fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,8 @@ const app = ({ Component, pageProps }): ReactNode => (
             <link rel="icon" href="/favicon.ico" />
             <meta charSet="utf-8" />
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet" />
             {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
             <script defer src="https://www.googletagmanager.com/gtag/js?id=UA-61523435-6"></script>
             <script defer src="/ga.js"></script>
